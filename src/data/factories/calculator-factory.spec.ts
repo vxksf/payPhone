0ff8ada31411d54cbe23c71d2c118097e364a0b3
Kelvin Sanches Felix <kelvin.felix@finnet.com.br>
2021-01-27 18:59:17 -0300

    feat: add test for factory

--- src/data/factories/calculator-factory.spec.ts
+++ src/data/factories/calculator-factory.spec.ts
@@ -3,22 +3,22 @@ import { Plan } from '../../domain/plans/plans'
 import { FaleMaisCalculator, TraditionalCallCalculator } from '../services/call-price-calculators'
 
 describe('CalculatorFactory Factory', () => {
-  test('Should return TraditionalCallCalculator when plan is traditional', async () => {
+  test('Should return TraditionalCallCalculator when plan is traditional', () => {
     const instance = CalculatorFactory.fromPlanType(Plan.TRADITIONAL)
     expect(instance).toBeInstanceOf(TraditionalCallCalculator)
   })
 
-  test('Should return FaleMaisCallCalculator when plan is FaleMais 30', async () => {
+  test('Should return FaleMaisCallCalculator when plan is FaleMais 30', () => {
     const instance = CalculatorFactory.fromPlanType(Plan.FALEMAIS30)
     expect(instance).toBeInstanceOf(FaleMaisCalculator)
   })
 
-  test('Should return FaleMaisCallCalculator when plan is FaleMais60 ', async () => {
+  test('Should return FaleMaisCallCalculator when plan is FaleMais60 ', () => {
     const instance = CalculatorFactory.fromPlanType(Plan.FALEMAIS60)
     expect(instance).toBeInstanceOf(FaleMaisCalculator)
   })
 
-  test('Should return FaleMaisCallCalculator when plan is FaleMais120 ', async () => {
+  test('Should return FaleMaisCallCalculator when plan is FaleMais120 ', () => {
     const instance = CalculatorFactory.fromPlanType(Plan.FALEMAIS120)
     expect(instance).toBeInstanceOf(FaleMaisCalculator)
   })
