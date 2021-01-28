dc2a8a2babf7e32d51e837bd6a416a155716f61a
Kelvin Sanches Felix <kelvin.felix@finnet.com.br>
2021-01-27 21:33:15 -0300

    feat: new features added

--- src/main/factories/calculate-call-price/calculate-call-price.ts
+++ src/main/factories/calculate-call-price/calculate-call-price.ts
@@ -1,15 +1,8 @@
-import { SignUpController } from '../../../presentation/controllers/signup/signup'
-import { DbAddAccount } from '../../../data/usecases/add-account/db-add-account'
-import { AccountMongoRepository } from '../../../infra/db/mongodb/account/account-mongo-repository'
-import { LogMongoRepository } from '../../../infra/db/mongodb/log/log-mongo-repository'
 import { Controller } from '../../../presentation/protocols'
-import { LogControllerDecorator } from '../../decorator/log'
-import { makeSignUpValidation } from './signup-validation'
+import { CalculateCallController } from '../../../presentation/controllers/calculate-call-price/calculate-call-price'
+import { CalculateCallPrice } from '../../../data/usecases/calculate-call-price/calculate-call-price'
 
 export const makeCalculateCallController = (): Controller => {
-  const accountMongoRepository = new AccountMongoRepository()
-  const dbAddAccount = new DbAddAccount(bcryptAdapter, accountMongoRepository)
-  const signUpController = new SignUpController(dbAddAccount, makeSignUpValidation())
-  const logMongoRepository = new LogMongoRepository()
-  return new LogControllerDecorator(signUpController,logMongoRepository)
+  const calculateCallPrice = new CalculateCallPrice()
+  return new CalculateCallController(calculateCallPrice)
 }
